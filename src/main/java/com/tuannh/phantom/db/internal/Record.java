fadf8d2510260f3c2aeee7ec28cb67a75c95652b
tuannh29 <tuannh27@viettel.com.vn>
2021-07-12 12:11:28 +0700

    add record

--- src/main/java/com/tuannh/phantom/db/internal/Record.java
+++ src/main/java/com/tuannh/phantom/db/internal/Record.java
@@ -1,61 +1,81 @@
 package com.tuannh.phantom.db.internal;
 
 import com.tuannh.phantom.commons.number.NumberUtils;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.zip.CRC32;
 
 public class Record {
+    /**
+     * Header
+     * key
+     * value
+     */
+    private static final int HEADER_SIZE = 1 + 1 + 4 + 8 + 4; // version(1), key_size(1), value_size(4), sequence_number(8), checksum(4)
+    private static final int HEADER_SIZE_WITHOUT_CHECKSUM = 1 + 1 + 4 + 8;
+    private static final int VERSION_OFFSET = 0;
+    private static final int KEY_SIZE_OFFSET = 1;
+    private static final int VALUE_SIZE_OFFSET = 1 + 1;
+    private static final int SEQUENCE_NUMBER_OFFSET = 1 + 1 + 4;
+    private static final int CHECKSUM_OFFSET = 1 + 1 + 4 + 8;
+
+    @Getter
+    private final Header header;
+    @Getter
     private final byte[] key;
+    @Getter
     private final byte[] value;
 
-    @NoArgsConstructor
+    public Record(byte[] key, byte[] value) {
+        this.key = key;
+        this.value = value;
+        this.header = new Header(Versions.DATA_FILE_VERSION, (byte) key.length, value.length, -1, 0);
+    }
+
+    public Record(byte[] key, byte[] value, Header header) {
+        this.key = key;
+        this.value = value;
+        this.header = header;
+    }
+
+    private long checksum(ByteBuffer buffer) {
+        CRC32 crc32 = new CRC32();
+        crc32.update(buffer.array(), 0, HEADER_SIZE_WITHOUT_CHECKSUM);
+        crc32.update(key);
+        crc32.update(value);
+        return crc32.getValue();
+    }
+
+    public boolean verifyChecksum() {
+        long checksum = checksum(header.serialize());
+        return checksum == header.checksum;
+    }
+
+    public ByteBuffer[] serialize() {
+        header.checksum = checksum(header.serialize()); // just for checksum, overwrite checksum later
+        ByteBuffer headerBuffer = header.serialize();
+        return new ByteBuffer[] {headerBuffer, ByteBuffer.wrap(key), ByteBuffer.wrap(value)};
+    }
+
+    public static Record deserialize(ByteBuffer buffer, Header header) {
+        buffer.flip();
+        byte[] key = new byte[header.keySize];
+        byte[] value = new byte[header.valueSize];
+        buffer.get(key);
+        buffer.get(value);
+        return new Record(key, value, header);
+    }
+
+    @AllArgsConstructor
     @Getter
     @Setter
     public static class Header {
-        private static final int HEADER_SIZE = 1 + 1 + 4 + 8 + 4; // version(1), key_size(1), value_size(4), sequence_number(8), checksum(4)
-        private static final int HEADER_SIZE_WITHOUT_CHECKSUM = 1 + 1 + 4 + 8;
-        private static final int VERSION_OFFSET = 0;
-        private static final int KEY_SIZE_OFFSET = 1;
-        private static final int VALUE_SIZE_OFFSET = 1 + 1;
-        private static final int SEQUENCE_NUMBER_OFFSET = 1 + 1 + 4;
-        private static final int CHECKSUM_OFFSET = 1 + 1 + 4 + 8;
-
         private byte version;
         private byte keySize;
         private int valueSize;
         private long sequenceNumber;
-        @Getter(AccessLevel.NONE)
-        @Setter(AccessLevel.NONE)
         private long checksum;
-
-        public Header(byte version, byte keySize, int valueSize, long sequenceNumber, long checksum) throws IOException {
-            this.version = version;
-            this.keySize = keySize;
-            this.valueSize = valueSize;
-            this.sequenceNumber = sequenceNumber;
-            ByteBuffer buffer = ByteBuffer.allocate(HEADER_SIZE);
-            buffer.put(VERSION_OFFSET, version);
-            buffer.put(KEY_SIZE_OFFSET, keySize);
-            buffer.putInt(VALUE_SIZE_OFFSET, valueSize);
-            buffer.putLong(SEQUENCE_NUMBER_OFFSET, sequenceNumber);
-            long calculatedChecksum = checksum(buffer);
-            if (checksum != calculatedChecksum) {
-                throw new IOException("checksum failed. record header corrupted");
-            }
-            this.checksum = checksum;
-        }
-
-        private long checksum(ByteBuffer buffer) {
-            CRC32 crc32 = new CRC32();
-            crc32.update(buffer.array(), 0, HEADER_SIZE_WITHOUT_CHECKSUM);
-            return crc32.getValue();
-        }
 
         public ByteBuffer serialize() {
             ByteBuffer buffer = ByteBuffer.allocate(HEADER_SIZE);
@@ -63,12 +83,11 @@ public class Record {
             buffer.put(KEY_SIZE_OFFSET, keySize);
             buffer.putInt(VALUE_SIZE_OFFSET, valueSize);
             buffer.putLong(SEQUENCE_NUMBER_OFFSET, sequenceNumber);
-            checksum = checksum(buffer);
             buffer.putInt(CHECKSUM_OFFSET, NumberUtils.fromUInt32(checksum));
             return buffer;
         }
 
-        public static Header deserialize(ByteBuffer buffer) throws IOException {
+        public static Header deserialize(ByteBuffer buffer) {
             byte version = buffer.get(VERSION_OFFSET);
             byte keySize = buffer.get(KEY_SIZE_OFFSET);
             int valueSize = buffer.getInt(VALUE_SIZE_OFFSET);
---
+++ src/main/java/com/tuannh/phantom/db/internal/Versions.java
@@ -0,0 +1,5 @@
+package com.tuannh.phantom.db.internal;
+
+public class Versions {
+    public static byte DATA_FILE_VERSION = (byte) 0xF1;
+}
